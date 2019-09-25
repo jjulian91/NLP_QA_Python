9c7cef7ba911934c071252d1cc3f15f14131df71
Jonathan Julian <jjulian2@mail.sfsu.edu>
2019-09-25 10:44:39 -0700

    Basic categorizing going on, May need to look deeper to our rules and how to define them

--- mainframe/mainframe.py
+++ mainframe/mainframe.py
@@ -1,6 +1,5 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
 from nltk.corpus import stopwords
-
 # DIFFERENT WAY OF IMPORTING
 # from nltk import tokenize
 # tokenize.sent_tokenize
@@ -9,10 +8,18 @@ counter = y = 0
 def main():
     
     while(1):
+        category = "blank"
         value = input('Enter in a question: ')
-        tokenized = word_tokenize(value)
-        print(f'tokenized words:    {tokenized}')
-        check_stopwords(tokenized)
+        tagged_sentence = word_Magic(value)
+        print(tagged_sentence)
+        for words in tagged_sentence:
+            if words[1] == "NNP" or words[1] == "NNPS":
+                category = "Name"
+            elif words[1] == 'WP' or words[1] == "WP$":
+                category = "Person"
+            elif words[1] == "WRB" or words[1] == "WDT":
+                category = "Location"
+        print(category)
         runstat()
 
 # this will be used later to measure our stastics on how accurate our program performs
@@ -36,6 +43,23 @@ def check_stopwords(tokenized):
 
 #     for list of tested stop words . REMOVE AFTER YOU SEE THIS.
 # https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
+
+def word_Magic(question):
+    tokenized = nltk.word_tokenize(question)
+    print(f'tokenized words:    {tokenized}')
+    # gives us stop words to run part of
+    # speech on to begin categorization.
+    #
+    # Stop Words may not be necessary
+    #
+    #
+    stop_words = set(stopwords.words('english'))
+    _stopwords = [words for words in tokenized if not words in stop_words]
+    print(f'stop words to check for:    {_stopwords}')
+    # End getting stop words.
+    postaggedwords = nltk.pos_tag(tokenized)
+    #print(f'these are tagged words:      {postaggedwords}')
+    return postaggedwords
 
 if __name__ == '__main__':
     main()
