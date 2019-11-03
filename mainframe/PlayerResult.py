793cb0c14cdadfb901b678efccbd0899a314b93d
Jonathan Julian <jjulian2@mail.sfsu.edu>
2019-11-02 17:50:17 -0700

    began implementing classes

---
+++ mainframe/PlayerResult.py
@@ -0,0 +1,15 @@
+class PlayerResult:
+
+    def __init__(self, name, startYear, endYear, position, height, weight, birthdate, college, city, country):
+        self.name = name
+        self.startYear = startYear
+        self.endYear = endYear
+        self.position= position
+        self.height = height
+        self.weight = weight
+        self.birthdate = birthdate
+        self.college = college
+        self.city = city
+        self.country = country
+
+    def somefunction(self, args):
